0b65fefc00338a2364e9f0bd0d599eb36ccbef7b
zombieJ <smith3816@gmail.com>
2013-11-24 18:25:30 +0800

    support change event

--- src/select/bootstrap-select.js
+++ src/select/bootstrap-select.js
@@ -13,7 +13,16 @@
 		var $field = $(this).closest(".btn-group").find("[data-toggle='dropdown'][data-type='selector']");
 		var $field_val = $field.find("[data-value]");
 		var $field_target = $($field.attr("data-to"));
+		var pre_val = $field_val.attr("data-value");
+		var pre_tgt_val = $field_target.val();
 		$field_val.val(_val).text(_text).attr("data-value", _val);
 		$field_target.val(_val);
+		
+		if(pre_val != _val) {
+			my.change();
+		}
+		if(pre_tgt_val != _val) {
+			$field_target.change();
+		}
 	});
 }(window.jQuery);
