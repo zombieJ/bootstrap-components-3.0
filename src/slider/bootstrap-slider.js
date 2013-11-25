a9089cdab75d9d6eb2a9583069d01c8c9c66413c
zombieJ <smith3816@gmail.com>
2013-11-26 00:30:05 +0800

    support multi node slider

--- src/slider/bootstrap-slider.js
+++ src/slider/bootstrap-slider.js
@@ -5,10 +5,15 @@
 */
 
 !function ($) {
+	function index(element) {
+		return element.parent().find("button").index(element);
+	}
 	function getLeft(element) {
 		return Number(element.css("margin-left").replace("px", ""));
 	}
-	function getValue(element) {
+
+
+	/*function getValue(element) {
 		var _total_left = 0;
 		var _element = $(element);
 		while(_element.length != 0) {
@@ -16,17 +21,20 @@
 			_element = _element.prev();
 		}
 		return _total_left;
-	}
+	}*/
 
 	var _instance = null;
 	var _mouseLeft = 0;
 	$(document).on("mousedown.bs.slider", "button[data-toggle='slider']", function(event){
 		_instance = $(this);
 		_mouseLeft = event.pageX - _instance.offset().left;
-		var _element = $(_instance.prev());
-		while(_element.length != 0) {
+
+		var _process = _instance.parent();
+		var _sliders = _process.find("button");
+		var _index = index(_instance);
+		for(var i = 0 ; i < _index ; i += 1) {
+			var _element = $(_sliders[i]);
 			_mouseLeft += _element.outerWidth() + getLeft(_element);
-			_element = _element.prev();
 		}
 	});
 	$(document).on("mouseup.bs.slider", function(event){
@@ -40,21 +48,41 @@
 			if(_min == null || isNaN(_min)) _min = 0;
 			if(_max == null || isNaN(_max)) _max = 100;
 
-			// set the position of silder
-			var _target = $(_instance.attr("data-to"));
-			var _pre_val = Number(_target.val());
+			// calculate enable range
 			var _process = _instance.parent();
+			var _sliders = _process.find("button");
+			var _len = _sliders.length;
+			var _index = index(_instance);
+			var _value = 0;
 			var _total_width = _process.outerWidth();
+			var _value_range = _total_width;
+			for(var i = 0 ; i < _index ; i += 1) {
+				var _element = $(_sliders[i]);
+				var _left = getLeft(_element);
+				var _width = _element.outerWidth();
+				_value_range -= _left + _width;
+				_total_width -= _width;
+				_value += _left;
+			}
+			_value_range -= _instance.outerWidth();
+			_total_width -= _instance.outerWidth();
+			for(var i = _index + 1 ; i < _len ; i += 1) {
+				var _element = $(_sliders[i]);
+				var _width = _element.outerWidth();
+				_value_range -= getLeft(_element) + _width;
+				_total_width -= _width;
+			}
+
 			var _instance_left = event.pageX - _process.offset().left - _mouseLeft;
-			_instance.parent().find("button").each(function() {
-				_total_width -= $(this).outerWidth();
-			});
 			if(_instance_left < 0) _instance_left = 0;
-			if(_instance_left > _total_width) _instance_left = _total_width;
+			if(_instance_left > _value_range) _instance_left = _value_range;
+			_value += _instance_left;
 			_instance.css("margin-left", _instance_left);
 
-			// set the value of slider
-			var _ptg = getValue(_instance) / _total_width;
+			// generate value
+			var _target = $(_instance.attr("data-to"));
+			var _pre_val = Number(_target.val());
+			var _ptg = _value / _total_width;
 			var _val = _min + (_max - _min) * _ptg;
 			_instance.val(_val).attr("data-value", _val);
 			_target.val(_val);
