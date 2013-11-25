c37361d8c9c59fb175edaca2b2a991ffb62890f5
zombieJ <smith3816@gmail.com>
2013-11-25 23:28:00 +0800

    support slider - left

--- src/slider/bootstrap-slider.js
+++ src/slider/bootstrap-slider.js
@@ -5,40 +5,56 @@
 */
 
 !function ($) {
+	function getLeft(element) {
+		return Number(element.css("margin-left").replace("px", ""));
+	}
+	function getValue(element) {
+		var _total_left = 0;
+		var _element = $(element);
+		while(_element.length != 0) {
+			_total_left += getLeft(_element);
+			_element = _element.prev();
+		}
+		return _total_left;
+	}
+
 	var _instance = null;
 	var _mouseLeft = 0;
 	$(document).on("mousedown.bs.slider", "button[data-toggle='slider']", function(event){
 		_instance = $(this);
 		_mouseLeft = event.pageX - _instance.offset().left;
+		var _element = $(_instance.prev());
+		while(_element.length != 0) {
+			_mouseLeft += _element.outerWidth() + getLeft(_element);
+			_element = _element.prev();
+		}
 	});
 	$(document).on("mouseup.bs.slider", function(event){
 		_instance = null;
 	});
 	$(document).on("mousemove.bs.slider", function(event){
 		if(_instance != null) {
+			// limit the range of min & max value
+			var _min = Number(_instance.parent().attr("data-min"));
+			var _max = Number(_instance.parent().attr("data-max"));
+			if(_min == null || isNaN(_min)) _min = 0;
+			if(_max == null || isNaN(_max)) _max = 100;
+
+			// set the position of silder
 			var _target = $(_instance.attr("data-to"));
 			var _pre_val = Number(_target.val());
 			var _process = _instance.parent();
-			var _min = Number(_instance.attr("data-min"));
-			var _max = Number(_instance.attr("data-max"));
-			var _process_width = _process.outerWidth();
-			var _instance_width = _instance.outerWidth();
-			var _total_width = _process_width - _instance_width;
+			var _total_width = _process.outerWidth();
 			var _instance_left = event.pageX - _process.offset().left - _mouseLeft;
-			if(_min == null || isNaN(_min)) {
-				_min = 0;
-			}
-			if(_max == null || isNaN(_max)) {
-				_max = 100;
-			}
-			if(_instance_left < 0) {
-				_instance_left = 0;
-			}
-			if(_instance_left > _total_width) {
-				_instance_left = _total_width;
-			}
+			_instance.parent().find("button").each(function() {
+				_total_width -= $(this).outerWidth();
+			});
+			if(_instance_left < 0) _instance_left = 0;
+			if(_instance_left > _total_width) _instance_left = _total_width;
 			_instance.css("margin-left", _instance_left);
-			var _ptg = _instance_left / _total_width;
+
+			// set the value of slider
+			var _ptg = getValue(_instance) / _total_width;
 			var _val = _min + (_max - _min) * _ptg;
 			_instance.val(_val).attr("data-value", _val);
 			_target.val(_val);
