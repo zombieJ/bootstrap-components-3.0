3b65377a6efc5ab96fb8fb95cf330d2ddad81182
zombieJ <smith3816@gmail.com>
2013-12-21 19:51:07 +0800

    good start

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,32 @@
 $(document).ready(function(){
+	var _nav;
 	$.get('_nav.html', function(data) {
-		$('body').prepend(data);
-		
-		$('body').scrollspy({ target: '#nav_bar' });
+		_nav = $(data);
+		$('body').prepend(_nav);
+
+		loadPage("gettingStarted", "Getting Started");
 	});
+
+	function loadPage(page, name) {
+		// add nav
+		var $li = $("<li>");
+		var $a = $("<a>");
+		$a.text(name);
+		$a.attr("href", "#" + page);
+		$li.append($a);
+		_nav.find("ul").append($li);
+
+		// add container
+		var $div = $("<div>");
+		$div.text("loading '" + name + "'...");
+		$(".container .row").append($div);
+
+		$.get('_' + page + '.html', function(data) {
+			var $content = $(data);
+			$content.find("h1").attr("id", page);
+			$div.addClass("chapter").html($content);
+
+			$('body').scrollspy({ target: '#nav_lst' });
+		});
+	}
 });
