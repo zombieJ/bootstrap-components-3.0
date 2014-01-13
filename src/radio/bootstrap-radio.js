f7385444c737c94db317820526c9b8a59fe69867
zombieJ <smith3816@gmail.com>
2014-01-13 21:14:01 +0800

    support disbaled radio

--- src/radio/bootstrap-radio.js
+++ src/radio/bootstrap-radio.js
@@ -37,45 +37,8 @@
 		var _label = $(this);
 		var _my = _label.find(".radio[data-toggle='radio']");
 		if(_my.length != 0) {			// find checkbox to go on
-			checkRadio(_my);
+			var _disabled = _my.attr("disabled") != null;
+			if(!_disabled) checkRadio(_my);
 		}
 	});
-
-	/*// change checkbox status
-	function updateStatus(_instance, _checked) {
-		if(_checked) {
-			_instance.attr("checked", "checked");
-		} else {
-			_instance.removeAttr("checked", "checked");
-		}
-	}
-
-	// update data to target status
-	function updateTarget(_target, _checked) {
-		if(_target != null) {
-			_target.prop("checked", _checked);
-		}
-	}
-
-	// click on the label
-	$(document).on("click.bs.radio", "label", function(event){
-		var _label = $(this);
-		var _my = _label.find(".radio[data-toggle='radio']");
-		var _checked = null;
-		if(_my.length != 0) {			// find radio to go on
-			var _target = $(_my.attr("data-to"));
-			var _radio = _label.find("input[type='radio']");
-			if(_radio.length != 0) {
-				_checked = _radio.prop("checked");
-			} else {
-				_checked = !(_my.attr("checked") != null);
-				updateTarget(_target, _checked);
-			}
-			updateStatus(_my, _checked);
-
-			// change event
-			_my.add(_target).change();
-		}
-	});
-	*/
 }(window.jQuery);
