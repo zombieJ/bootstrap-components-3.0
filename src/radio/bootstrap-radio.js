616c707180d6a02e30560b29cfcbd67f984df5cc
zombieJ <smith3816@gmail.com>
2014-01-13 21:46:43 +0800

    support uncheck remove val

--- src/radio/bootstrap-radio.js
+++ src/radio/bootstrap-radio.js
@@ -3,18 +3,59 @@
 */
 
 !function ($) {
+	$.fn.extend({
+		radio:function(options){
+			// get options
+			var _my = $(this);
+			var vars = $._bc.vars(options);
+			var _options = vars.options;
+			var _checked = $._bc.get(_options, "checked", null);
+			var _to = $._bc.get(_options, "to", null);
+				var _target = _to != null ? $(_to) : $(_my.attr("data-to"));
+
+			// set target element
+			if(_to != null) {
+				_my.attr("data-to", _to);
+			}
+
+			// set the value of radio and it will change target element too.
+			if(_checked === true) {
+				checkRadio(_my);
+			} else if(_checked === false) {
+				uncheckRadio(_my);
+			}
+		}
+	});
 	// change radios status
 	function checkRadio(_instance) {
 		if(_instance.attr("checked") != null) return;
 
 		// update all the radios
 		var _name = _instance.attr("name");
-		var _radios = $(".radio[data-toggle='radio'][name='" + _name + "']");
-		_radios.removeAttr("checked");
+		cleanRadios(_name);
 		_instance.attr("checked", "checked");
 
+		updateTarget(_instance);
+	}
+	// remove radios status
+	function uncheckRadio(_instance) {
+		if(_instance.attr("checked") == null) return;
+
+		// update all the radios
+		var _name = _instance.attr("name");
+		cleanRadios(_name);
+
+		updateTarget(_instance, "");
+	}
+	// clean radios
+	function cleanRadios(_name) {
+		var _radios = $(".radio[data-toggle='radio'][name='" + _name + "']");
+		_radios.removeAttr("checked");
+	}
+	// update target input
+	function updateTarget(_instance, _val) {
 		// update target element
-		var _val = _instance.attr("data-value");
+		_val = _val == null ? _instance.attr("data-value") : _val;
 		var _target = $(_instance.attr("data-to"));
 		var _pre_val = _target.val();
 		_target.val(_val);
