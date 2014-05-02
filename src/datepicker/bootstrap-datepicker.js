e131aff3fed2dd6c123a48863500bbbd135a6d2d
zombieJ <smith3816@gmail.com>
2014-05-02 11:14:28 +0800

    fix bug of clicking will trigger datapicker refresh

--- src/datepicker/bootstrap-datepicker.js
+++ src/datepicker/bootstrap-datepicker.js
@@ -171,6 +171,10 @@ $._bc.vals.datepicker.index = 1;
 	$(document).on("click.bs.datepicker", "[data-toggle='datepicker']", function(event){
 		_preventEvent = true;
 		var my = $(this);
+
+		// Skip refresh if container work for the same element
+		if($(_instance).data("trigger") === this) return;
+
 		var _target = $(my.attr("data-to"));
 			var target = _target.length != 0 ? _target : my;
 		var _container = $(my.attr("data-container"));
@@ -216,6 +220,7 @@ $._bc.vals.datepicker.index = 1;
 
 		// generate datepicker component
 		var $container = $('<div class="bsc-datepicker">');
+			$container.data("trigger", this);
 		var $yearpicker = $('<div class="yearpicker picker-group">');
 			var $yearpicker_header = $('<div class="picker-header">');
 				var $yearpicker_header_year_minus = $('<button class="btn btn-default minus" type="button">');
