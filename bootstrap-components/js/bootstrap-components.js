08f620d83229c4d0cee7d0b34eb86e8309f779d4
zombieJ <smith3816@gmail.com>
2014-06-05 23:21:00 +0800

    pkg

--- bootstrap-components/js/bootstrap-components.js
+++ bootstrap-components/js/bootstrap-components.js
@@ -1631,22 +1631,38 @@ $.extend({
 
 			var _name = data.name;
 			var _list = data.list || [];
+			var _enabled = data.enabled !== false;
+			var _open = _enabled ? data.open !== false : data.open === true;
 
 			var $ul = _my.is("ul") ? _my : $("<ul class='treeView'>").appendTo(_my);
 			var $li = $("<li>").appendTo($ul);
 			var $a = $("<a class='tree-icon glyphicon'>").appendTo($li);
-			var $name = $("<span>").html(_name).insertAfter($a);
+			var $name = (data.url ? $("<a>").attr("href", data.url) : $("<span>")).html(_name).insertAfter($a);
 
+			if(!_enabled) $li.addClass("disabled");
+
+			// Generate as list
 			if(_list.length) {
+				var cls_folder_open = options.folderOpenIcon || 'glyphicon-folder-close';
+				var cls_folder_close = options.folderCloseIcon || 'glyphicon-folder-open';
+
 				$a.attr("data-toggle", "tree")
-				.addClass("glyphicon-folder-open");
+				.attr("data-icon-open", cls_folder_open)
+				.attr("data-icon-close", cls_folder_close)
+				.addClass("glyphicon")
+				.addClass(_open ? cls_folder_close : cls_folder_open);
 
 				var $sub_ul = $("<ul class='tree-list'>").appendTo($li);
 				$.each(_list, function(i, data) {
 					$sub_ul.tree(data, options);
 				});
+				if(!_open) {
+					$sub_ul.hide();
+				}
 			} else {
-				$a.addClass("glyphicon-file");
+				var cls_file = options.itemIcon || 'glyphicon-file';
+
+				$a.addClass(cls_file);
 			}
 			return _my;
 		},
@@ -1656,10 +1672,19 @@ $.extend({
 		event.preventDefault();
 
 		var _my = $(this);
+		if(_my.closest("li").hasClass("disabled")) return;
+
 		var $list = _my.parent().find("> .tree-list");
+		var clsOpen = _my.attr("data-icon-open");
+		var clsClose = _my.attr("data-icon-close");
+
 		if($list.is(":hidden")) {
+			_my.removeClass(clsOpen);
+			_my.addClass(clsClose);
 			$list.slideDown();
 		} else {
+			_my.addClass(clsOpen);
+			_my.removeClass(clsClose);
 			$list.slideUp();
 		}
 	});
